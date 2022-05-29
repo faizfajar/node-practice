98c1d1662b2a521c8a8cbb52e4e916567c6a53b9
faizfajar <faizaprianda112@gmail.com>
2022-05-29 18:55:10 +0700

    commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9
+
+if(amount < 10){
+    console.log('ini adalah angka kecil');
+}else{
+    console.log('ini adalah angka besar ');
+}
+
+console.log(`ini project node pertama saya`);
---
+++ 2-global.js
@@ -0,0 +1,11 @@
+// GLOBAL - NO WINDOW
+// __dirname - path current dirrectory
+// __filename - file name 
+// require - function to use module (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being execute
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('hello world')
+}, 1000);
