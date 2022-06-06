b4002c39de5c6451a74f1cd9aaf6d3559a6ffae8
faizfajar <faizaprianda112@gmail.com>
2022-06-06 12:01:34 +0700

    day3

---
+++ 8-npm/app.js
@@ -0,0 +1,13 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
+
+
+// console.log(validator.isEMail('faizfajar@gmail.com'));
+// console.log(validator.isMobilePhone('083856321','id-ID'));
+// console.log(validator.isNumeric('08385632aa1'));
+
+// console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+console.log(chalk.italic.bgBlueBright.bold('Hello Word'))
+const pesan = 'Hello Word';
+console.log(chalk.italic.yellow.bold(pesan))
